6b0b6f26fe297d6a8082014be368027262e14557
angcastillo18 <angcastillo18@gmail.com>
2020-11-18 23:43:08 -0500

    added logout function

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,11 +5,24 @@ import {
   faBars,
   faTimes,
   faGraduationCap,
+  faUserCircle,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
 export default function NavBar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+  //*logout
+  const handleLogout = async () => {
+    try {
+      await logout();
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -65,24 +78,56 @@ export default function NavBar() {
                   <span className=''>Cursos</span>
                 </Link>
               </li>
-              <li className='nav-item m-2'>
-                <Link
-                  className='py-2  px-3 lg:py-1 xl:text-lg xl:py-2  rounded-full bg-white border-yellow-500 border-2 flex items-center justify-center text-base font-bold leading-snug text-white hover:opacity-75'
-                  to='/register'
-                  onClick={() => setNavbarOpen(!navbarOpen)}
-                >
-                  <span className='text-black'>Crear cuenta</span>
-                </Link>
-              </li>
-              <li className='nav-item m-2'>
-                <Link
-                  className=' py-2 px-4 lg:py-1 xl:text-lg xl:py-2 rounded-full bg-black flex items-center justify-center text-base font-bold leading-snug text-white hover:opacity-75'
-                  to='/login'
-                  onClick={() => setNavbarOpen(!navbarOpen)}
-                >
-                  <span className='text-yellow-500'>Iniciar Sesión</span>
-                </Link>
-              </li>
+              {currentUser ? (
+                <li className='dropdown inline-block relative my-auto lg:ml-2 xl:ml-2'>
+                  <button className='bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center'>
+                    <FontAwesomeIcon
+                      className='mr-2 text-gray-500 text-2xl '
+                      icon={faUserCircle}
+                    />
+                    <h5>{currentUser.email.split("@")[0].toUpperCase()}</h5>
+                  </button>
+                  <ul className='dropdown-menu absolute hidden text-gray-700 pt-1'>
+                    <li>
+                      <Link
+                        className='rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
+                        to='/user/mycourses'
+                      >
+                        <span>Mis Cursos</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap cursor-pointer'
+                        onClick={() => handleLogout()}
+                      >
+                        <span>Cerrar sesión</span>
+                      </button>
+                    </li>
+                  </ul>
+                </li>
+              ) : (
+                <>
+                  <li className='nav-item m-2'>
+                    <Link
+                      className='py-2  px-3 lg:py-1 xl:text-lg xl:py-2  rounded-full bg-white border-yellow-500 border-2 flex items-center justify-center text-base font-bold leading-snug text-white hover:opacity-75'
+                      to='/register'
+                      onClick={() => setNavbarOpen(!navbarOpen)}
+                    >
+                      <span className='text-black'>Crear cuenta</span>
+                    </Link>
+                  </li>
+                  <li className='nav-item m-2'>
+                    <Link
+                      className=' py-2 px-4 lg:py-1 xl:text-lg xl:py-2 rounded-full bg-black flex items-center justify-center text-base font-bold leading-snug text-white hover:opacity-75'
+                      to='/login'
+                      onClick={() => setNavbarOpen(!navbarOpen)}
+                    >
+                      <span className='text-yellow-500'>Iniciar Sesión</span>
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
